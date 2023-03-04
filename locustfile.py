4d76d05f8edd8071865449c48fa12bf73fab635b
ACHRAF-by <achraf.benyahya98@gmail.com>
2023-03-04 13:43:39 +0100

    stress test data fix

--- locustfile.py
+++ locustfile.py
@@ -6,12 +6,13 @@ class LoadTestUser(HttpUser):
     def test_post(self):
         headers = { 'Content-Type': 'application/json'}
         data = {
-            'title': 'Ghost in the Shell',
-            'gender': ['Action'],
-            'description': 'In this post-cyberpunk iteration of a possible future, computer technology has advanced to the point that many members of the public possess cyberbrains, technology that allows them to interface their biological brain with various networks',
-            'type': 0,
-            'producer': 'Koichi Yuri',
-            'studio': 'Production I.G'
+            'Title': 'Ghost in the Shell',
+            'Gender': ['Action', 'Adventure'],
+            'Synopsis': 'In this post-cyberpunk iteration of a possible future, computer technology has advanced to the point that many members of the public possess cyberbrains, technology that allows them to interface their biological brain with various networks',
+            'Type': "TV",
+            'Producer': 'Koichi Yuri',
+            'Studio': 'Production I.G',
+            "Source": "Manga"
         }
         self.client.post('/api/prediction', headers=headers, json=data)
 
